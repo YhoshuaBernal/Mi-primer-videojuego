120ec305c5382ee28bf23f115ef2bdfc34ebade4
YhoshuaBernal <yhoshuamail@gmail.com>
2024-02-19 23:11:12 -0600

    new maps

--- js/maps.js
+++ js/maps.js
@@ -23,27 +23,39 @@ maps.push(`
   -XXXXXXXXX
   OXXXXXXXXX
 `);
-// maps.push(`
-//   O--XXXXXXX
-//   X--XXXXXXX
-//   XX----XXXX
-//   X--XX-XXXX
-//   X-XXX--XXX
-//   X-XXXX-XXX
-//   XX--XX--XX
-//   XX--XXX-XX
-//   XXXX---IXX
-//   XXXXXXXXXX
-//   `);
-// maps.push(`
-//   I-----XXXX
-//   XXXXX-XXXX
-//   XX----XXXX
-//   XX-XXXXXXX
-//   XX-----XXX
-//   XXXXXX-XXX
-//   XX-----XXX
-//   XX-XXXXXXX
-//   XX-----OXX
-//   XXXXXXXXXX
-// `);
+maps.push(`
+  O--XXXXXXX
+  X--XXXXXXX
+  XX----XXXX
+  X--XX-XXXX
+  X-XXX--XXX
+  X-XXXX-XXX
+  XX--XX--XX
+  XX--XXX-XX
+  XXXX---IXX
+  XXXXXXXXXX
+  `);
+maps.push(`
+  I-----XXXX
+  XXXXX-XXXX
+  XX----XXXX
+  XX-XXXXXXX
+  XX-----XXX
+  XXXXXX-XXX
+  XX-----XXX
+  XX-XXXXXXX
+  XX-----OXX
+  XXXXXXXXXX
+`);
+maps.push(`
+----------
+-X-XX-XXX-
+-X----XXX-
+-X-X-XXXX-
+-X-----X--
+-XXX---X-X
+-X---X-X-X
+-XX-X-X--X
+IX-----OXX
+XXXXXXXXXX
+`);
